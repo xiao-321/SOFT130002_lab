7a7398a97575f2cc317e738cc376ab4597fcad27
xiao-321 <2339860032@qq.com>
2020-05-14 21:59:51 +0800

    lab7

---
+++ lab7/Lab07设计文档.md
@@ -0,0 +1,21 @@
+# Lab07设计文档
+
+李晓伟 19302010006
+
+先将一块给写完 然后通过循环 将整个html页面完成
+
+先创建一个container作为存放内容的div 根据css className为item 下面的同理
+
+Tips部分 用.innerText设置字 用.style.padding设置下边距
+
+Inner_box1部分又分为Author和Lifetime两部分 字号分别为h3和h4
+设置Author的右边距 并且用.style.display = "inline"将两个放在同一行
+用.appendChild（）将其放进Inner_box1
+
+Inner_box2部分 用一个for循环 将photo逐个加进去
+
+Button部分 .textContent设置内容为visit
+
+最后将四个部分都放进先前的container就完成了一个container 
+
+进行循环 完成整个html
--- lab7/lab07.js
+++ lab7/lab07.js
@@ -4,3 +4,51 @@ const works = [
     { author: "John Herry UY", lifetime:"1894-1928",tips: "Abstract", photos: ["abstract1.jpg","abstract2.jpg","abstract3.jpg","abstract4.jpg","abstract5.jpg"] },
     { author: "Coco",lifetime:"1777-1799", tips: "Beauty",  photos: ["beauty1.jpg","beauty2.jpg"] }
 ];
+
+function display(number) {
+    var container = document.createElement("div");
+    container.className="item";
+
+    var Tips = document.createElement("p");
+    Tips.innerText = "Genre:" + works[number].tips;
+    Tips.style.padding = "0 0 10px 0";
+
+    var Inner_box1 = document.createElement("div");
+    Inner_box1.className = "inner-box";
+    var Author = document.createElement("h3");
+    Author.innerText = works[number].author;
+    Author.style.display = "inline";
+    Author.style.padding = "0 10px 0 0";
+    Inner_box1.appendChild(Author);
+    var Lifetime = document.createElement("h4");
+    Lifetime.innerText = "lifetime:" + works[number].lifetime;
+    Lifetime.style.display = "inline";
+    Inner_box1.appendChild(Lifetime);
+
+    var Inner_box2 = document.createElement("div");
+    Inner_box2.className = "inner-box";
+    var PopularPhotos = document.createElement("h3");
+    PopularPhotos.innerText="Popular Photos";
+    Inner_box2.appendChild(PopularPhotos);
+
+    for(var i = 0;i < works[number].photos.length;i++){
+        var img = document.createElement("img");
+        img.className = "photo";
+        img.src = works[number].photos[i];
+        Inner_box2.appendChild(img);
+    }
+
+    var Button = document.createElement("button");
+    Button.textContent = "visit";
+
+    container.appendChild(Tips);
+    container.appendChild(Inner_box1);
+    container.appendChild(Inner_box2);
+    container.appendChild(Button);
+
+    document.body.appendChild(container);
+}
+
+for(var j = 0;j < works.length;j ++) {
+    display(j);
+}
