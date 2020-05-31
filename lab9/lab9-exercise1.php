c15e5221439d8bb488e31356bed0e698f2e3dbce
xiao-321 <2339860032@qq.com>
2020-05-31 20:51:40 +0800

    lab9

--- lab9/lab9-exercise1.php
+++ lab9/lab9-exercise1.php
@@ -9,9 +9,12 @@
 <h1>PHP section (inside &lt;?php ... ?&gt; tags)</h1>
 <?php
 //this is a php comment IN tags (will not appear)
-echo "This was output using PHP";
-echo "<br>"; //notice we must echo tags in php.
-
+date_default_timezone_set("Asia/Shanghai");
+$date = date("l, M dS, Y , H:i:s");
+echo "This page was generated: " . $date . "<hr/>";
+$remaining = 365 + date("L");
+$remaining = 365 - date("z");
+echo "There are ". $remaining . " days left in the year";
 ?>
 </body>
 </html>
